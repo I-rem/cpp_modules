552086dad15b790abf49bb39f55f1c5ed6a86a20
IremK <ayseirem.kayacioglu@gmail.com>
2023-08-30 17:02:24 +0300

    Add megaphone.cpp

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 
-int main()
+int main(int argc, char **argv)
 {
-	
+	if (argc > 1)
+	{
+		int i = 1;
+		int j = 0;
+		while (argv[i])
+		{
+			while(argv[i][j])
+			{
+				argv[i][j] = toupper(argv[i][j]);
+				std::cout << argv[i][j];
+				j++;
+			}
+			i++;
+		}
+	}
+	else
+		std::cout<< "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	std::cout << std::endl;
+	return (0);
 }
