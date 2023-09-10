6ee9881401cf76a21ea01ffc42252ade57bd19c7
IremK <ayseirem.kayacioglu@gmail.com>
2023-09-11 00:45:42 +0300

    ex04 we can copy the ifs to ofs without replacements

--- 01/ex04/main.cpp
+++ 01/ex04/main.cpp
@@ -5,14 +5,23 @@ int main(int argc, char **argv)
 {
     if (argc == 4)
     {
-        std::string filename = argv[1];
         std::string s1 = argv[2];
         std::string s2 = argv[3];
 
-        std::ifstrean ifs(filename);
+        std::ifstream ifs(argv[1]);
+        if (!ifs.is_open())
+            return 1;
         std::ofstream ofs("test.out");
-        ofs.close();
+        if (!ofs.is_open())
+            return 1;
+
+        std::string str;
+        while (std::getline(ifs, str))
+            ofs << str << std::endl;
+
         ifs.close();
+        ofs.close();
     }
-    return 0;
+    else
+        return 1;
 }
