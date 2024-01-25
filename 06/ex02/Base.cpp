1d1f8ff1877d16f628dee6e4cadfa771ca922075
IremK <ayseirem.kayacioglu@gmail.com>
2024-01-25 14:37:20 +0300

    Ex02 done and tested

--- 06/ex02/Base.cpp
+++ 06/ex02/Base.cpp
@@ -1,11 +1,64 @@
-#include "Base.h"
+#include "Base.hpp"
 
-Base::Base()
+
+Base::~Base() {}
+
+Base *generate(void)
 {
-	
+	std::srand(std::time(0));
+
+	switch (std::rand() % 3)
+	{
+	case 0:
+		return (new A());
+
+	case 1:
+		return (new B());
+
+	case 2:
+		return (new C());
+	}
+	return (NULL);
 }
 
-Base::~Base()
+void identify(Base *p)
 {
-
+	if (dynamic_cast<A*>(p))
+		std::cout << "A" << std::endl;
+	else if (dynamic_cast<B*>(p))
+		std::cout << "B" << std::endl;
+	else if (dynamic_cast<C*>(p))
+		std::cout << "C" << std::endl;
+	else
+		std::cout << "Error" << std::endl;
 }
+
+void identify(Base &p)
+{
+	try {
+		A &a = dynamic_cast<A&>(p);
+		std::cout << "A" << std::endl;
+		(void)a;
+		return;
+	}
+	catch (std::exception &bc) {}
+
+	try {
+		B &b = dynamic_cast<B&>(p);
+		std::cout << "B" << std::endl;
+		(void)b;
+		return;
+	}
+	catch (std::exception &bc) {}
+
+	try {
+		C &c = dynamic_cast<C&>(p);
+		std::cout << "C" << std::endl;
+		(void)c;
+		return;
+	}
+	catch (std::exception &bc) {
+		std::cout << "Error" << std::endl;
+	}
+}
+
--- 06/ex02/Base.hpp
+++ 06/ex02/Base.hpp
@@ -1,10 +1,18 @@
-#ifndef BASE_HPP
-#define BASE_HPP
+#pragma once
 
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
 class Base {
 public:
-    virtual ~Base() {}
+    virtual ~Base();
 };
 
-#endif
+class A : public Base {};
+class B : public Base {};
+class C : public Base {};
+
+Base *generate(void);
+void identify(Base* p);
+void identify(Base& p);
 
