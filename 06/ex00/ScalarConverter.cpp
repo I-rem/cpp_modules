98fd45d3bd1834a423d0ceeff4cdaee22034145a
IremK <ayseirem.kayacioglu@gmail.com>
2024-02-20 01:21:21 +0300

    Actually fix scalarconerter

--- 06/ex00/ScalarConverter.cpp
+++ 06/ex00/ScalarConverter.cpp
@@ -16,39 +16,42 @@ ScalarConverter &ScalarConverter::operator=(const ScalarConverter &rhs)
 
 ScalarConverter::~ScalarConverter() {}
 
-const std::string ScalarConverter::toUpperCase(const std::string &var)
-{
-	std::string tmp = var;
-	for (int i = var.length() - 1; i >= 0; i--)
-		tmp[i] = toupper(tmp[i]);
-	return tmp;
-}
-
 bool ScalarConverter::isNan(const std::string &str)
 {
-	return (toUpperCase(str) == "NAN");
+	return ((str) == "nan" || (str) == "+inff" || (str) == "-inff");
 }
 
 void ScalarConverter::toChar(const std::string &str)
 {
 	char c = 0;
-	if (isNan(str)){
+	std::string res = str;
+	if (str.length() == 3 && str[0] == '\'' && str[2] == '\'')
+	{
+		res.erase(0,1);
+		res.erase(2,1);
+	}
+	if (isNan(str))
+	{
 		std::cout << "char: impossible" << std::endl;
 		return;
 	}
+
 	try{
-		c = static_cast<char>(atoi(str.c_str()));
-	}catch (std::exception &e){
-		if (str.length() == 1 && isprint(str[0]))
-			c = str[0];
+		c = static_cast<char>(atoi(res.c_str()));
+	}
+	catch (std::exception &e){
+		if (res.length() == 1 && isprint(res[0]))
+			c = res[0];
 		std::cout << "int : Non displayable" << std::endl;
 	}
+
 	std::cout << "char :";
-  if (isprint(c)) {
+	if (res.length() == 1 && isprint(res[0]))
+			c = res[0];
+	if (isprint(c) && !(c >= '0' && c <= '9'))
       std::cout << "'" << std::string(1, c).c_str() << "'";
-  } else {
+	else
       std::cout << "Non Displayable";
-  }
   std::cout << std::endl;
 }
 
@@ -65,13 +68,18 @@ void ScalarConverter::toInt(const std::string &str)
 		n = atoi(str.c_str());
 	}catch (std::exception &e){
 		if (str.length() == 1 && isprint(str[0]))
-    {
+    	{
 			printNumber(str[0], 'i');
 			return;
 		}
 		std::cout << "int: Non displayable" << std::endl;
 		return;
 	}
+	if (str.length() == 1 && isprint(str[0]) && !(str[0] >= '0' && str[0] <= '9'))
+    	{
+			printNumber(str[0], 'i');
+			return;
+		}
 	std::cout << "int : " << n << std::endl;
 }
 
@@ -79,19 +87,24 @@ void ScalarConverter::toFloat(const std::string &str)
 {
 	float n = 0;
 	if (isNan(str)){
-		std::cout << "float :nan" << std::endl;
+		std::cout << "float :nanf" << std::endl;
 		return;
 	}
 	try{
 		n = static_cast<float>(atof(str.c_str()));
 	}catch (std::exception &e){
-		if(str.length() == 0 && isprint(str[0])){
+		if(str.length() != 0 && isprint(str[0])){
 			printNumber(str[0], 'f');
 			return;
 		}
 		std::cout << "Float Non Displayable" << std::endl;
 		return;
 	}
+	if (str.length() == 1 && isprint(str[0]) && !(str[0] >= '0' && str[0] <= '9'))
+    	{
+			printNumber(str[0], 'f');
+			return;
+		}
 	if (n == static_cast<int>(n))
 		std::cout << "float :" << n << ".0f" << std::endl;
 	else
@@ -102,15 +115,23 @@ void ScalarConverter::toDouble(const std::string &str)
 {
 	double n = 0;
 	if(isNan(str))
+	{
 		std::cout << "double: impossible" << std::endl;
+		return ;
+	}
 	try{
 		n = atof(str.c_str());
 	}catch (std::exception &e){
-		if (str.length() == 0 && isprint(str[0]))
+		if (str.length() != 0 && isprint(str[0]))
 			printNumber(str[0], 'd');
 		std::cout << "double : impossible " << std::endl;
 		return;
 	}
+	if (str.length() == 1 && isprint(str[0]) && !(str[0] >= '0' && str[0] <= '9'))
+    	{
+			printNumber(str[0], 'd');
+			return;
+		}
 	if (n == static_cast<int>(n))
 		std::cout << "double : " << n << ".0" << std::endl;
 	else
